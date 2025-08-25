944173525f00abf86405a1a265a9e85e5c78c39d
rutaku12rutaku12 <chookyuho@gmail.com>
2025-08-25 15:12:18 +0900

    페이징 완료

--- src/main/java/web/controller/PostController.java
+++ src/main/java/web/controller/PostController.java
@@ -1,17 +1,57 @@
 package web.controller;
 
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import web.model.dto.PageDto;
+import web.model.dto.PostDto;
 import web.service.PostService;
 
 
-@RestController
-@RequestMapping("/post")
+@RestController // (1) HTTP 요청/응답 자료 매핑 기술
+@RequestMapping("/post") // (2) HTTP URL 매핑 기술
+@RequiredArgsConstructor // (3) final 변수에 대한 자동 생성자 주입
 public class PostController {
-    @Autowired private PostService postService;
+
+    private final PostService postService; // @RequiredArgsConstructor 사용함으로 @Autowired 생략 한다.
 
     // [1] 게시물 등록
+    @PostMapping("") // method : post , url : localhost:8080/post , body : { btitle : , bcontetn : , cno : , pno }
+    public int writePost(@RequestBody PostDto postDto , HttpSession session){
+        // 1. 현재 로그인 상태 확인
+        Object login = session.getAttribute("loginMno");
+        // 2. 비로그인이면 등록 실패처리
+        if( login == null ) return 0;
+        // 3. 로그인이면 현재 로그인한 회원번호을 postDto 대입하기
+        int mno = (int)login;
+        postDto.setMno(mno);
+        // 4. 서비스 호출 하고 응답 반환 하기
+        return postService.writePost( postDto );
+    } // func end
 
-}
+    // [2] 게시물 전체 조회
+    @GetMapping("")
+    // method : GET, URL : localhost:8080/post?cno=1&page=1&count=5  // 1번 카테고리의 1페이지의 5개 게시물
+    // method : GET, URL : localhost:8080/post?cno=1&page=2&count=10  // 1번 카테고리의 2페이지의 10개 게시물
+    public PageDto findAllPost(@RequestParam( defaultValue = "1" ) int cno ,
+                               @RequestParam( defaultValue = "1" ) int page ,
+                               @RequestParam( defaultValue = "5" ) int count ){
+        // 만약에 URL 주소상의 지정한 쿼리스트링 매개변수가 없으면 defaultValue 속성으로 기본값 대입 할 수 있다.
+        // cno : 카테고리번호 , page : 현재페이지번호 , count : 페이지당 게시물수
+        return postService.findAllPost(cno,page,count);
+    }
+} // class end
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/web/model/dao/PostDao.java
+++ src/main/java/web/model/dao/PostDao.java
@@ -1,13 +1,101 @@
 package web.model.dao;
 
 import org.springframework.stereotype.Repository;
+import web.model.dto.PostDto;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
-public class PostDao {
+public class PostDao extends Dao{
 
     // [1] 게시물 등록
-//    public int writePost(){
-//        return;
-//    };
+    public int writePost(PostDto postDto){
+        try{// 1. SQL 작성
+            String sql = "insert into post( ptitle, pcontent, cno, mno ) values (?,?,?,?)";
+            // 2. SQL 기재 , PreparedStatement.RETURN_GENERATED_KEYS : SQL 실행시 자동 생성된 PK 반환
+            PreparedStatement ps = conn.prepareStatement( sql , Statement.RETURN_GENERATED_KEYS );
+            // 3. SQL 매개변수 대입
+            ps.setString(1,postDto.getPtitle());
+            ps.setString(2,postDto.getPcontent());
+            ps.setInt(3,postDto.getCno());
+            ps.setInt(4,postDto.getMno());
+            // 4. SQL 실행
+            int count = ps.executeUpdate();
+            // 5. SQL 결과에 따른 로직/리턴/확인
+            if( count == 1){
+                ResultSet rs = ps.getGeneratedKeys(); // SQL 실행후 자동 생성된 PK 반환
+                if( rs.next()) return rs.getInt(1); // 등록 성공시 첫번째 pk 속성 값 반환
+            }
+        }catch (Exception e){System.out.println(e);}
+        return 0; // 등록 실패시
+    } // func end
 
-}
+    // [2-1] 조회할 정보의 개수 ,
+    public int getTotalCount( int cno ){
+        try{// 1. SQL 작성
+            String sql = "select count(*) from post where con = ? "; // count(*) : 레코드 전체수 sql 함수
+            // 2. SQL 기재
+            PreparedStatement ps = conn.prepareStatement(sql);
+            // 3. SQL 매개변수 대입
+            ps.setInt(1,cno);
+            // 4. SQL 실행
+            ResultSet rs = ps.executeQuery();
+            // 5. SQL 결과에 따른 로직/리턴/확인
+            if( rs.next() ){
+                return rs.getInt(1); // 첫번째 레코드의 속성값 1개 반환
+            }
+        }catch (Exception e){System.out.println(e);}
+            return 0; // 조회 결과 없으면 0 반환
+    } // func end
+
+    // [2-2] 게시물 전체 조회
+    public List<PostDto> findAll( int cno , int startRow , int count ) {
+        List<PostDto> list = new ArrayList<>();
+        try{// 1. SQL 작성
+            String sql = "select * from post p inner join member m on p.mno= m.mno where p.cno = ? order by p.pno desc limit ?, ?;";
+            // 2. SQL 기재
+            PreparedStatement ps = conn.prepareStatement(sql);
+            // 3. SQL 매개변수 대입
+            ps.setInt(1,cno);
+            ps.setInt(2,startRow);
+            ps.setInt(3,count);
+            // 4. SQL 실행
+            ResultSet rs = ps.executeQuery();
+            // 5. SQL 결과에 따른 로직/리턴/확인
+            while ( rs.next() ){
+                PostDto postDto = new PostDto();
+                postDto.setMno(rs.getInt("mno"));
+                postDto.setCno(rs.getInt("cno"));
+                postDto.setPcontent(rs.getString("pcontent"));
+                postDto.setPdate(rs.getString("pdate"));
+                postDto.setPview(rs.getInt("pview"));
+                postDto.setPno(rs.getInt("pno"));
+                postDto.setPtitle(rs.getString("ptitle"));
+                postDto.setMid(rs.getString("mid")); // member 테이블과 join한 결과 mid 호출 가능하다.
+                list.add( postDto );
+            }
+        }catch (Exception e){System.out.println(e);}
+        return list;
+    }
+
+
+
+} // class end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
