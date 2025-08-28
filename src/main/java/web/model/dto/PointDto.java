da289e69fd3dbd31c22d959bd3790d4db6864a09
rutaku12rutaku12 <chookyuho@gmail.com>
2025-08-29 00:26:16 +0900

    sql수정/ 포인트 dto생성

---
+++ src/main/java/web/model/dto/PointDto.java
@@ -0,0 +1,15 @@
+package web.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor@AllArgsConstructor
+@Data
+public class PointDto {
+    private int plno;
+    private int mno;
+    private int plpoint;
+    private String plcomment;
+    private String pldate;
+}
